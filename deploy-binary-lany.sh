57dcc06c6c402222365b9060296d223d0f8ccbdf
Liam <98386642+liam-ob@users.noreply.github.com>
2024-06-20 08:34:42 +0800

    editted out errors

--- deploy-binary-lany.sh
+++ deploy-binary-lany.sh
@@ -9,14 +9,34 @@ if id "$username" &>/dev/null; then
     echo "User $username already exists."
     exit 1
 fi
-read -s -p "Enter the password of the new user: " password
 useradd -m -s /bin/bash -c "Binary Lane User" $username
-echo -e "$password\n$password" | passwd "$username"
+passwd "$username"
 # Add user to sudo group
 usermod -aG sudo $username
 # switch to user
 su - $username
 
 # Install the latest version of python
-sudo apt-get update
-sudo apt-get upgrade python3
+sudo apt-get update -qq
+sudo apt-get install python3.11 -y -qq
+
+# Install nginx
+sudo apt-get install nginx -y -qq
+
+# Install curl
+sudo apt-get install curl -y -qq
+
+# Install poetry 
+curl -sSL https://install.python-poetry.org | python3.11 -
+sudo ln -s $HOME/.local/bin/poetry /usr/bin/poetry 
+if command -v poetry &> /dev/null
+then
+    echo "Poetry is installed."
+else
+    echo "Poetry is NOT installed!"
+fi
+sudo poetry config virtualenvs.create false
+sudo poetry config virtualenvs.in-project true
+# Repeat becasue sometimes it doesnt work?
+poetry config virtualenvs.create false
+poetry config virtualenvs.in-project true
